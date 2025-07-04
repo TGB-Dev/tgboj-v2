feb39dc45265be669caa54466b15dd576fdd791e
Hoàng Minh Thiên <hoangminhthien05022009@gmail.com>
2025-07-04 11:08:34 +0700

    fix: wrong problem and submission points

--- judge/management/commands/export_event_feed.py
+++ judge/management/commands/export_event_feed.py
@@ -62,21 +62,17 @@ def fill_problem(contest: Contest, root: ET.Element) -> Dict[int, int]:
             index = (index - 1) // 26
         return ret[::-1]
 
-    def get_points_for_problem(index):
-        # Assuming all problems have the same points, which is the case in most contests
-        return str(contest.format.config.get("points", 100))
-
     contest_problems = contest.contest_problems.order_by("order").values_list(
-        "problem__id", "problem__name"
+        "problem__id", "problem__name", "points"
     )
     problem_index = {}
-    for id, (external_id, name) in enumerate(contest_problems, start=1):
+    for id, (external_id, name, points) in enumerate(contest_problems, start=1):
         problem = ET.SubElement(root, "problem")
         problem.tail = "\n"
         ET.SubElement(problem, "id").text = str(id)
         ET.SubElement(problem, "label").text = get_label_for_problem(id)
         ET.SubElement(problem, "name").text = name
-        ET.SubElement(problem, "score").text = get_points_for_problem(id)
+        ET.SubElement(problem, "score").text = points
 
         problem_index[external_id] = id
 
@@ -139,7 +135,7 @@ def fill_run(
         ET.SubElement(run, "judged").text = "True"
         ET.SubElement(run, "result").text = sub.result
         ET.SubElement(run, "solved").text = "True" if sub.result == "AC" else "False"
-        ET.SubElement(run, "score").text = str(sub.points or 0)
+        ET.SubElement(run, "score").text = str(sub.case_points or 0)
 
         hash = (sub.problem.id, sub.user.user.id)
         if sub.result == "AC":
